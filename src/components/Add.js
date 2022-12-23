514a30a691449ba7097f414bba6857323f10ad12
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2022-12-23 11:00:34 -0600

    no change

---
+++ src/components/Add.js
@@ -0,0 +1,11 @@
+import {useState, UseEffect} from 'react'
+
+const Add = () => {
+    return (
+        <>
+            <h1>Hello Add World</h1>
+        </>
+    )
+}
+
+export default Add
---
+++ src/components/Edit.js
@@ -0,0 +1,11 @@
+import {useState, useEffect} from 'react'
+
+const Edit = (props) => {
+    return (
+        <>
+            <h1>Hello Edit World</h1>
+        </>
+    )
+}
+
+export default Edit
